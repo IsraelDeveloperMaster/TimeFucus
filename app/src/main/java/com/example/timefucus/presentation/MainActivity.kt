073a12f75627b334ede507ba49e2e424afcdf3aa
Israel Santos <dev@developermaster.net>
2025-03-01 00:18:35 +0100

    Refactor: Remove initial files and add timer session logic
    - Removed `Color.kt`, `MainActivity.kt`, `Theme.kt`, `Type.kt`.
    - Added `LocalStorageRepository` interface and `LocalStorageRepositoryImpl` implementation.
    - Added `GetTimerSessionByDateUseCase` and `SaveTimerSessionUseCase`.
    - Created `HomeScreenViewModel` to manage timer and session logic.
    - Implemented `HomeScreen` composable for timer interaction.
    - Created new composables: `AutoResizedText`, `BorderedIcon`, `CircleDot`, `CustomButton`, `InformationItem`, `TimerTypeItem`.
    - Created `AppDatabase`, `TimerSessionEntity`, and `TimerSessionDao` for local data storage.
    - Added `DatabaseModule` and `RepositoryModule` for dependency injection.
    - Implemented `MainActivity` to display the `HomeScreen`.
    - Created `Dimens` data class and `FocusTimerYTTheme` for UI theming.
    - Created `Color`, and `Type` for UI theming.
    - Added `FocusTimerApp` for application setup.
    - Added `Constants` for project constants.
    - Added `Resource` class for data state handling.
    - Created `TimerSessionModel`, and `TimerTypeEnum`.
    - Added font files.

---
+++ app/src/main/java/com/example/timefucus/presentation/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.timefucus.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.ui.Modifier
+import com.cirilobido.focustimeryt.presentation.home.HomeScreen
+import com.cirilobido.focustimeryt.presentation.theme.FocusTimerYTTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val windowSize = calculateWindowSizeClass(this)
+            FocusTimerYTTheme(
+                windowSize = windowSize.widthSizeClass
+            ) {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.surface
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
